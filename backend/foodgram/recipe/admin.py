fc401a146cc73797e91d09a5ae16d7647d12e858
Farruh Rustamov <frustamov@openwaygroup.com>
2024-03-21 16:03:59 +0100

    recipe admin.py update

--- backend/foodgram/recipe/admin.py
+++ backend/foodgram/recipe/admin.py
@@ -11,6 +11,7 @@ class RecipeAdmin(admin.ModelAdmin):
     )
     list_filter = ('author', 'name', 'tags')
     filter_horizontal = ('tags', 'ingredients')
+    search_fields = ['name', 'author__username']
 
     def favorite_count(self, obj):
         return Favorite.objects.filter(recipe_id=obj.id).count()
@@ -18,14 +19,18 @@ class RecipeAdmin(admin.ModelAdmin):
 
 @admin.register(Ingredient)
 class IngredientAdmin(admin.ModelAdmin):
-    list_display = (
-        'id', 'name', 'measurement_unit',
-    )
+    list_display = ('id', 'name', 'measurement_unit',)
     list_filter = ('name',)
+    search_fields = ['name']
 
 
 @admin.register(Tag)
 class TagAdmin(admin.ModelAdmin):
-    list_display = (
-        'id', 'name', 'color', 'slug',
-    )
+    list_display = ('id', 'name', 'color', 'slug',)
+    search_fields = ['name', 'slug']
+
+
+@admin.register(Favorite)
+class FavoriteAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'recipe')
+    search_fields = ['user__username', 'user__email']
